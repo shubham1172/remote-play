fa88e39b00d4f5ce0809a4667c19d42d25725857
Shubham Sharma <shubhash@microsoft.com>
2021-05-03 08:58:51 +0530

    Fix eslint issues
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- static/index.js
+++ static/index.js
@@ -1,16 +1,17 @@
-const ws = new WebSocket("ws://" + location.host + "/ws");
-let start = { x: 0, y: 0 };
+const Hammer = window.hammer;
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.getElementById("touchpad").addEventListener("touchstart", touchStart);
-  document.getElementById("touchpad").addEventListener("touchmove", touchMove);
+const ws = new WebSocket(`ws://${window.location.host}/ws`);
+const start = { x: 0, y: 0 };
 
-  var hammer = new Hammer(document.getElementById("touchpad"));
-  hammer.on("tap", function (ev) {
-    // left mouse button click
-    ws.send(JSON.stringify({ type: "tap" }));
-  });
-});
+function callEndpoint(endpoint) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('get', endpoint, true);
+  xhr.setRequestHeader(
+    'Content-Type',
+    'application/x-www-form-urlencoded; charset=UTF-8',
+  );
+  xhr.send();
+}
 
 function touchStart(ev) {
   ev.preventDefault();
@@ -20,21 +21,26 @@ function touchStart(ev) {
 
 function touchMove(ev) {
   ev.preventDefault();
-  offsetX = ev.changedTouches[0].clientX - start.x;
-  offsetY = ev.changedTouches[0].clientY - start.y;
+  const offsetX = ev.changedTouches[0].clientX - start.x;
+  const offsetY = ev.changedTouches[0].clientY - start.y;
 
-  ws.send(JSON.stringify({ type: "move", x: offsetX * 2, y: offsetY * 2 }));
+  ws.send(JSON.stringify({ type: 'move', x: offsetX * 2, y: offsetY * 2 }));
 
   start.x = ev.changedTouches[0].clientX;
   start.y = ev.changedTouches[0].clientY;
 }
 
-function callEndpoint(endpoint) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("get", endpoint, true);
-  xhr.setRequestHeader(
-    "Content-Type",
-    "application/x-www-form-urlencoded; charset=UTF-8"
-  );
-  xhr.send();
-}
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('touchpad').addEventListener('touchstart', touchStart);
+  document.getElementById('touchpad').addEventListener('touchmove', touchMove);
+
+  const hammer = new Hammer(document.getElementById('touchpad'));
+  hammer.on('tap', () => {
+    // left mouse button click
+    ws.send(JSON.stringify({ type: 'tap' }));
+  });
+});
+
+export default {
+  callEndpoint,
+};
