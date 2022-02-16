4453b8640371ec81aa65c66b44ea0ea50f5176aa
tesla-srt <71569052+tesla-srt@users.noreply.github.com>
2022-02-15 22:50:28 -0500

    PR for Issue #27 (#29)
    * PR for Issue #27
    
    * Updated Commit For Issue #27
    
    * -Updated readme
    -Readability in index.html
    
    * -Removed line break for conisitency.
    -Updated screenshot to reflect changes

--- static/index.js
+++ static/index.js
@@ -3,10 +3,10 @@ const start = { x: 0, y: 0 };
 
 function callEndpoint(endpoint) {
   const xhr = new XMLHttpRequest();
-  xhr.open('get', endpoint, true);
+  xhr.open("get", endpoint, true);
   xhr.setRequestHeader(
-    'Content-Type',
-    'application/x-www-form-urlencoded; charset=UTF-8',
+    "Content-Type",
+    "application/x-www-form-urlencoded; charset=UTF-8"
   );
   xhr.send();
 }
@@ -22,19 +22,34 @@ function touchMove(ev) {
   const offsetX = ev.changedTouches[0].clientX - start.x;
   const offsetY = ev.changedTouches[0].clientY - start.y;
 
-  ws.send(JSON.stringify({ type: 'move', x: offsetX * 2, y: offsetY * 2 }));
+  ws.send(JSON.stringify({ type: "move", x: offsetX * 2, y: offsetY * 2 }));
 
   start.x = ev.changedTouches[0].clientX;
   start.y = ev.changedTouches[0].clientY;
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementById('touchpad').addEventListener('touchstart', touchStart);
-  document.getElementById('touchpad').addEventListener('touchmove', touchMove);
-
-  const hammer = new Hammer(document.getElementById('touchpad'));
-  hammer.on('tap', (ev) => {
+document.addEventListener("DOMContentLoaded", event => {
+  document.getElementById("touchpad").addEventListener("touchstart", touchStart);
+  document.getElementById("touchpad").addEventListener("touchmove", touchMove);
+  var singleTap = new Hammer.Tap({
+    event: "tap",
+    taps: 1
+  });
+  var doubleTap = new Hammer.Tap({
+    event: "doubletap",
+    taps: 2
+  });
+  const hammer = new Hammer.Manager(document.getElementById("touchpad"));
+  hammer.add([doubleTap, singleTap]);
+  doubleTap.recognizeWith(singleTap);
+  doubleTap.requireFailure(singleTap);
+  singleTap.requireFailure(doubleTap);
+  hammer.on("tap", ev => {
     // left mouse button click
-    ws.send(JSON.stringify({ type: 'tap' }));
+    ws.send(JSON.stringify({ type: "tap" }));
+  });
+  hammer.on("doubletap", ev => {
+    // right mouse button click
+    ws.send(JSON.stringify({ type: "doubletap" }));
   });
 });
