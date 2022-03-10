48883a733f4fbda17e9bab0ae2c197cf647f21c3
idofish <idof2911@gmail.com>
2022-03-10 14:29:28 +0200

    PR for #34 (#37)
    * Handling general exceptions in websocket_endpoint and closing the websocket only when needed
    
    * Fixed a typo
    
    * Update main.py
    
    Co-authored-by: Shubham Sharma <shubhash@microsoft.com>
    
    * websocket_endpoint printing error in red. Console.log uses f-strings
    
    Co-authored-by: Shubham Sharma <shubhash@microsoft.com>

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ write colorful messages to STDOUT.
 def log(msg, *args, end='\n', color=None):
     """
     Logs msg % args to the console.
-    color can be g (green), c (cyan)
+    color can be g (green), c (cyan), r (red)
     """
     # use https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
     # to add codes as needed.
@@ -16,7 +16,9 @@ def log(msg, *args, end='\n', color=None):
         code = 32
     elif color == 'c':
         code = 96
+    elif color == 'r':
+        code = 31
     if code > 0:
-        print("%s%s%s" % ('\x1b[%dm' % code, msg % args, '\x1b[39m'), end=end)
+        print(f"\x1b[{code}m{msg % args}\x1b[39m", end=end)
     else:
         print(msg % args, end=end)
